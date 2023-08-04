7a69733477a5ca1fa9c6fef5494ec0ce2c021f20
Mathanraj <mathanraj.kanini@gmail.com>
2023-08-04 22:00:19 +0530

    KTW [BE} - Added Controllers of WishList API

---
+++ Backend/KaniniTourismWebsite/KTWWishlistAPI/Controllers/WishListController.cs
@@ -0,0 +1,85 @@
+﻿using KTWWishlistAPI.Interfaces;
+using KTWWishlistAPI.Models;
+using KTWWishlistAPI.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KTWWishlistAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class WishListController : ControllerBase
+    {
+        private readonly IUserAction _userAction;
+
+        public WishListController(IUserAction userAction)
+        {
+            _userAction = userAction;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(WishList), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<WishList?>> Add(WishList wishList)
+        {
+            try
+            {
+                var result = await _userAction.AddWishList(wishList);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Delete(WishlistDTO wishlistDTO)
+        {
+            try
+            {
+                var result = await _userAction.DeleteWishList(wishlistDTO);
+                if (result != null)
+                {
+                    return Accepted();
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(List<WishList>),StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<List<WishList>?>> GetAllWishListBuUser(WishlistDTO wishlistDTO)
+        {
+            try
+            {
+                var result = await _userAction.GetAllWishListByuserID(wishlistDTO);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+    }
+}

41abf6ccb09fb3716a0323fedc42beb01a39df4c
Mathanraj <mathanraj.kanini@gmail.com>
2023-08-04 21:58:34 +0530

    KTW [BE} - Added Models of WishList API

---
+++ Backend/KaniniTourismWebsite/KTWWishlistAPI/Models/DTOs/WishlistDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace KTWWishlistAPI.Models.DTOs
+{
+    public class WishlistDTO
+    {
+        public int UserId { get; set; }
+        public int WishlistId { get; set; }
+    }
+}
---
+++ Backend/KaniniTourismWebsite/KTWWishlistAPI/Models/WishList.cs
@@ -0,0 +1,9 @@
+﻿namespace KTWWishlistAPI.Models
+{
+    public class WishList
+    {
+        public int WishListId { get; set; }
+        public int UserId { get; set; }
+        public int PackageId { get; set; }
+    }
+}

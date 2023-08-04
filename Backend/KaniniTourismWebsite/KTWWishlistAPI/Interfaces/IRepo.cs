c88562bc30b56631ed7044762af014da617ed3fd
Mathanraj <mathanraj.kanini@gmail.com>
2023-08-04 21:59:26 +0530

    KTW [BE} - Added InterFaces of WishList API

---
+++ Backend/KaniniTourismWebsite/KTWWishlistAPI/Interfaces/IRepo.cs
@@ -0,0 +1,14 @@
+﻿using KTWWishlistAPI.Models;
+
+namespace KTWWishlistAPI.Interfaces
+{
+    public interface IRepo
+    {
+        public Task<ICollection<WishList>?> GetAll();
+        public Task<WishList?> Add(WishList wishList);
+        public Task<int> Delete(int id);
+
+
+
+    }
+}
---
+++ Backend/KaniniTourismWebsite/KTWWishlistAPI/Interfaces/IUserAction.cs
@@ -0,0 +1,12 @@
+﻿using KTWWishlistAPI.Models;
+using KTWWishlistAPI.Models.DTOs;
+
+namespace KTWWishlistAPI.Interfaces
+{
+    public interface IUserAction
+    {
+        public Task<WishList?> AddWishList(WishList wishList);
+        public Task<ICollection<WishList>?> GetAllWishListByuserID(WishlistDTO wishlistDTO );
+        public Task<int> DeleteWishList(WishlistDTO wishlistDTO);
+    }
+}
